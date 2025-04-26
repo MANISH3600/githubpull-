7a7fd69ae06b5010993477efed8ed0da2e6bafa3
Manish Gupta <guptamanish1006@gmail.com>
2025-04-27 00:05:42 +0530

    testingagain1

--- webhook_manager/urls.py
+++ webhook_manager/urls.py
@@ -5,5 +5,5 @@ from .views import setup_webhook, github_webhook
 urlpatterns = [
     path('setup/', setup_webhook, name='setup_webhook'),
     path('webhook/', github_webhook, name='webhook'),
-    path('accounts/', include('accounts.urls')),
+
 ]
